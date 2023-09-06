86ecf672446f91a17772920a26c31693144a664f
MimHe <mimhe1381@gmail.com>
2023-09-06 22:24:39 +0330

    Add User Model

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+module.exports = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 30,
+        trim: true,
+        set: (v) => v[0].toUpperCase() + v.slice(1)
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (v) => validator.isEmail(v),
+            message: "Your email is not valid."
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: [{
+            validator: (v) => !/.* .*/.test(v),
+            message: "Your password is not valid."
+        }, {
+            validator: (v) => validator.isStrongPassword(v),
+            message: "Your password is not strong enough."
+        }]
+    },
+    birthdate: Date
+});
