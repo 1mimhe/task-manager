ec6588538fa624db429bbf044490503b3dc538fc
MimHe <mimhe1381@gmail.com>
2023-09-10 11:42:25 +0330

    Update user model

--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,7 @@ module.exports = mongoose.model('User', {
     email: {
         type: String,
         required: true,
+        unique: true,
         validate: {
             validator: (v) => validator.isEmail(v),
             message: "Your email is not valid."
