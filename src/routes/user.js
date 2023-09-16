e96a56686cb9c8e25a2053d20a67218c1971dd1a
MimHe <mimhe1381@gmail.com>
2023-09-16 13:02:07 +0330

    add authentication part

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
 
 router.post('/register', async (req, res) => {
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) return res.status(400).send('User already registered.');
 
-        user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            isAdmin: req.body.isAdmin
-        });
+        user = new User({...req.body});
 
         const result = await user.save();
         res.status(201).send(result);
     } catch (e) {
-        res.status(400).send(e);
+        res.status(500).send(e.message);
     }
+});
+
+router.post('/login', async (req, res) => {
+   try {
+       const user = await User.findOne({ email: req.body.email });
+       if (!user) return res.status(400).send('Invalid email or password.');
+
+       const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+       if (!isValidPassword) return res.status(400).send('Invalid email or password.');
+
+       const token = user.generateAuthToken();
+       res.header('X-Auth-Token', token).send(_.omit(user, ['password']));
+   } catch (e) {
+       res.status(500).send(e.message);
+   }
 });
 
 router.get('/', async (req, res) => {
