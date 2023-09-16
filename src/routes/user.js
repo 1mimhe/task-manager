6ff7067b3088318861bc27eeb4d91e9b110e816d
MimHe <mimhe1381@gmail.com>
2023-09-16 12:14:07 +0330

    Update register endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/', async (req, res) => {
-    const newUser = new User(req.body);
-
+router.post('/register', async (req, res) => {
     try {
-        const result = await newUser.save();
+        let user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(400).send('User already registered.');
+
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            isAdmin: req.body.isAdmin
+        });
+
+        const result = await user.save();
         res.status(201).send(result);
     } catch (e) {
         res.status(400).send(e);
@@ -39,7 +47,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.patch('/:id',  async (req, res) => {
+router.patch('/:id', async (req, res) => {
     const allowedUpdates = ['name', 'email', 'password', 'birthdate'];
     const isValidOperation = Object.keys(req.body).every((update) => allowedUpdates.includes(update));
 
@@ -47,7 +55,7 @@ router.patch('/:id',  async (req, res) => {
         res.status(400).send(new Error('Invalid update!'));
 
     try {
-        const result = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const result = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
         res.send(result);
     } catch (e) {
         res.status(500).send(e);
