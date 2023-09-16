2ec0058f7ec186f3c06d80e68ea4ad3aecd25c0a
MimHe <mimhe1381@gmail.com>
2023-09-17 00:29:02 +0330

    Update task route

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/task');
+const auth = require('../middlewares/auth');
 
-router.post('/', async (req, res) => {
-    const newTask = new Task(req.body);
+router.post('/', auth, async (req, res) => {
+    const newTask = new Task({ owner: req.user._id , ...req.body });
 
     try {
         const result = await newTask.save();
@@ -13,11 +14,22 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
-   try {
-       const tasks = await Task.find({});
+// GET /tasks?pageSize=&pageNumber=
+// default: pageSize=5, pageNumber=1
+router.get('/', auth, async (req, res) => {
+    const pageSize = Number(req.query.pageSize) || 5;
+    const pageNumber = Number(req.query.pageNumber) || 1;
 
-       if (!tasks)
+    try {
+       const tasks = await req.user.populate({
+               path: 'tasks',
+               options: {
+               skip: (pageNumber - 1) * pageSize,
+               limit: pageSize
+               }
+           });
+
+       if (!tasks.length)
            res.status(404).send();
 
        res.send(tasks);
@@ -26,9 +38,9 @@ router.get('/', async (req, res) => {
    }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
    try {
-       const task = await Task.findById(req.params.id);
+       const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
 
        if (!task)
            res.status(404).send();
@@ -39,7 +51,7 @@ router.get('/:id', async (req, res) => {
    }
 });
 
-router.patch('/:id',  async (req, res) => {
+router.patch('/:id', auth, async (req, res) => {
     const allowedUpdates = ['description', 'completed', 'dueDate'];
     const isValidOperation = Object.keys(req.body).every((update) => allowedUpdates.includes(update));
 
@@ -47,16 +59,16 @@ router.patch('/:id',  async (req, res) => {
         res.status(400).send(new Error('Invalid update!'));
 
     try {
-        const result = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const result = await Task.findByIdAndUpdate(req.params.id, { owner: req.user._id , ...req.body }, { new: true, runValidators: true });
         res.send(result);
     } catch (e) {
         res.status(500).send(e);
     }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
         if (!task)
             res.status(404).send();
